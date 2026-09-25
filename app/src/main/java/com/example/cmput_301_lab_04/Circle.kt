a31f41efc32720a351fa02f796e4ff4234295a6b
Ben <cadets.ben@gmail.com>
2026-09-25 16:27:16 -0600

    polishing touches

---
+++ app/src/main/java/com/example/cmput_301_lab_04/Circle.kt
@@ -0,0 +1,13 @@
+package com.example.cmput_301_lab_04
+
+/*  Do you like to see code comments?
+    Would you like to see more of them?
+    Consider hiring me to your team.
+    C, Kotlin, Python, SQL, VBA
+    Auth: Ben, 8259754321
+*/
+
+class Circle:Shape(){
+    override val x = 1
+    override val y = x
+}
--- app/src/main/java/com/example/cmput_301_lab_04/Shape.kt
+++ app/src/main/java/com/example/cmput_301_lab_04/Shape.kt
@@ -1,8 +1,17 @@
 package com.example.cmput_301_lab_04
 
+import androidx.compose.ui.graphics.Color
+
+
 abstract class Shape {
     abstract val x: Int
     abstract val y: Int
 
-    private val color: String = "purple"
+    private val color: String = "dildo purple" // We're all adults
 }
+
+
+
+
+
+
