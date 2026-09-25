15bc7dd43e715645943ae073bf1f17ce98c0f0eb
pain000000 <maricelmadrona21@gmail.com>
2026-09-24 18:23:40 -0600

    added madronaCircle.kt

--- app/src/main/java/com/example/cmput_301_lab_04/Shape.kt
+++ app/src/main/java/com/example/cmput_301_lab_04/Shape.kt
@@ -3,9 +3,5 @@ package com.example.cmput_301_lab_04
 abstract class Shape {
     abstract val x: Int
     abstract val y: Int
-}
-
-class madronaCircle: Shape() {
-    override val x = 200
-    override val y = 200
+    val color: String = "green"
 }
