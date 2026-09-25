1d93d1d380855b7cdcade49f13468b1be97e4a75
AbdurRabSiddiqui <156171531+AbdurRabSiddiqui@users.noreply.github.com>
2026-09-24 18:30:31 -0600

    change

--- app/src/main/java/com/example/cmput_301_lab_04/Circle.kt
+++ app/src/main/java/com/example/cmput_301_lab_04/Circle.kt
@@ -1,8 +1,8 @@
 package com.example.cmput_301_lab_04
 
 
-class Circle(val radius: Double, override val y: Int): Shape("Circle") {
+class Circle(val radis: Double, override val y: Int): Shape("Circle") {
 
     override val x: Double
-        get() = Math.PI * radius * radius
+        get() = Math.PI * radis * radis
 }
